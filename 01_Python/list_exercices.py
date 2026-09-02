4772e854e11f7b8cda49dc49d9955390f1c5b268
justabel4 <abel2.mampakakasongo@live.uwe.ac.uk>
2026-09-02 06:10:48 +0100

    exo 4

--- 01_Python/list_exercices.py
+++ 01_Python/list_exercices.py
@@ -1,4 +1,13 @@
-enemies = ["cat,", "dog", "fish"]
-print(enemies)
-print(enemies[1])
-print(enemies[-1])
+inventory = ["key", "wallet", "phone", "paper", "pen"]
+print(inventory)
+print(inventory[0])
+print(inventory[-1])
+
+inventory.append("coffee")
+inventory.remove("paper")
+
+print(len(inventory))
+print(inventory)
+
+for item in inventory:
+    print(inventory)
