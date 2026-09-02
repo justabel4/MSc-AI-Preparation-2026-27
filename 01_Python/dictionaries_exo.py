077298e27602bbc504a5754c9bcfb62b8d3c5f59
justabel4 <abel2.mampakakasongo@live.uwe.ac.uk>
2026-09-02 06:28:45 +0100

    exo 5 corrected

--- 01_Python/dictionaries_exo.py
+++ 01_Python/dictionaries_exo.py
@@ -7,14 +7,15 @@ player = {
     "enemies_defeated": 5
 }
 
-for item in player:
-    print(player["name"])
-    print(player["score"])
-    player["health"] -=25
-    print("health after damage", player["health"])
-    player["score"] += 500
-    print("new score", player["score"])
-    player["weapon"] = "riffle"
-    print(player["weapon"])
-    print(player)
+print(player["name"])
+print(player["score"])
 
+player["health"] -=25
+print("health after damage", player["health"])
+
+player["score"] += 500
+print("new score", player["score"])
+
+player["weapon"] = "riffle"
+print(player["weapon"])
+print(player)
