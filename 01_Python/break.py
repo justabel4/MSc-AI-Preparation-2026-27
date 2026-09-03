8682aa5cf0dbd86d769a527815b41fce0c127380
justabel4 <abel2.mampakakasongo@live.uwe.ac.uk>
2026-09-03 04:37:50 +0100

    exo 14 edit

--- 01_Python/break.py
+++ 01_Python/break.py
@@ -1,14 +1,17 @@
 valid_input = True
 
 while valid_input == True:
-    try:
-        score = int(input("enter 5:"))
 
-        if score == 5:
-            break
-        else:
-            if score < 0:
+    try:
+        score = int(input("enter score: "))
+
+        if score < 0:
                 print("score cannot be neagtive")
+        else:
+             print("score:", score)
+             break
+        
     except ValueError:
         print("invalid number, try again")
+
 print("loop finished")
