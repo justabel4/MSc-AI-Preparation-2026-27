f04afceca7e49b1daffbe13565075a2dee05ab02
justabel4 <abel2.mampakakasongo@live.uwe.ac.uk>
2026-09-03 04:12:56 +0100

    exo 12 edit

--- 01_Python/input_validation.py
+++ 01_Python/input_validation.py
@@ -1,9 +1,12 @@
 name = input("enter player name:")
-print("name:", name)
+
 try:
     level = int(input("enter level:"))
-    print ("level:", level)
     score = int(input("enter score:"))
+    print("name:", name)
+    print ("level:", level)
     print ("score:", score)
+    
 except ValueError:
     print("please enter a number")
+
