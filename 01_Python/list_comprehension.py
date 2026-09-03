805f3b699f8d10d3a7b853fdff3f499829188a87
justabel4 <abel2.mampakakasongo@live.uwe.ac.uk>
2026-09-03 22:57:52 +0100

    edit

--- 01_Python/list_comprehension.py
+++ 01_Python/list_comprehension.py
@@ -1,13 +1,9 @@
 scores = [1200, -50, 1800, -20, 900, 2100]
 
-valid_scores = []
-high_scores = []
-doubled_scores = []
 
-for number in scores:
-    valid_scores = [number for number in scores if number > 0]
-    high_scores = [ number for number in scores if number >= 1500]
-    doubled_scores = [number * 2 for number in valid_scores]
+valid_scores = [number for number in scores if number >= 0]
+high_scores = [ number for number in scores if number >= 1500]
+doubled_scores = [number * 2 for number in valid_scores]
 
 print("valid scores: ", valid_scores)
 print("high scores: ", high_scores)
